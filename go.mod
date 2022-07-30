d301d50126fab05e5f02066444ee89456573ba06
Tobi Lehman <mail@tobilehman.com>
2022-07-29 22:49:41 -0700

    add static assets

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tlehman/go-game
 go 1.18
 
 require (
+	crawshaw.io/sqlite v0.3.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/echo/v4 v4.7.2 // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
--- main.go
+++ main.go
@@ -7,14 +7,20 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
+func initEcho() *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Static("/static", "static")
 	e.GET("/", hello)
+	return e
+}
+func main() {
+	e := initEcho()
+	// Start the server
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "hello go gamer")
+	return c.String(http.StatusOK, "OK")
 }
